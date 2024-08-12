71e972f8e713e1c9d908f2fae789be8777ce33e8
batlovb <bogdan200255@gmail.com>
2024-08-12 16:32:21 +0300

    init

---
+++ config/build/babel/buildBabelLoader.ts
@@ -0,0 +1,38 @@
+import { BuildOptions } from "../types/types";
+import { removeDataTestIdBabelPlugin } from "./removeDataTestIdBabelPlugin";
+
+export function buildBabelLoader(option: BuildOptions) {
+  const { isDev, isProd } = option;
+
+  const plugins = [];
+
+  if (isProd) {
+    plugins.push([
+      removeDataTestIdBabelPlugin,
+      {
+        props: ["data-testid"],
+      },
+    ]);
+  }
+
+  return {
+    test: /\.tsx?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: [
+          "@babel/preset-env",
+          "@babel/preset-typescript",
+          [
+            "@babel/preset-react",
+            {
+              runtime: isDev ? "automatic" : "classic",
+            },
+          ],
+        ],
+        plugins: plugins.length ? plugins : undefined,
+      },
+    },
+  };
+}
---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,63 @@
+import { ModuleOptions } from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/types";
+import { buildBabelLoader } from "./babel/buildBabelLoader";
+
+export function buildLoaders(options: BuildOptions): ModuleOptions["rules"] {
+  const { isDev } = options;
+
+  const assetLoader = {
+    test: /\.(png|jpg|jpeg|gif)$/i,
+    type: "asset/resource",
+  };
+
+  const svgrLoader = {
+    test: /\.svg$/,
+    issuer: /\.[jt]sx?$/,
+    use: [
+      {
+        loader: "@svgr/webpack",
+        options: {
+          icon: true,
+          typescript: true,
+          svgoConfig: {
+            plugins: [
+              {
+                name: "convertColors",
+                params: {
+                  currentColor: true,
+                },
+              },
+            ],
+          },
+        },
+      },
+    ],
+  };
+
+  const cssLoaderWithModules = {
+    loader: "css-loader",
+    options: {
+      modules: {
+        namedExport: false,
+        exportLocalsConvention: "as-is",
+        localIdentName: isDev
+          ? "[path][name]__[local]--[hash:base64:5]"
+          : "[hash:base64:8]",
+      },
+    },
+  };
+
+  const scssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      cssLoaderWithModules,
+      "sass-loader",
+    ],
+  };
+
+  const babelLoader = buildBabelLoader(options);
+
+  return [assetLoader, scssLoader, babelLoader, svgrLoader];
+}
